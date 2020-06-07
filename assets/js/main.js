507b32f124d36fd8807c0eb3c71fb997aa0d960c
Summer <salvadin2@gmail.com>
2020-06-07 01:40:59 -0400

    CORS wasnt the issue, i hit a rate limit lol

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,9 @@
     window.initialize = () => {
         // Automagically grab my repositories and populate #repos with them.
         let repoList = document.getElementById('repos');
-        window.fetch('https://api.github.com/users/alerithe/repos').then(response => {
+        window.fetch('https://api.github.com/users/alerithe/repos', {
+            cache: 'no-cache'
+        }).then(response => {
             repoList.children[0].remove();
 
             if (response.ok) {
