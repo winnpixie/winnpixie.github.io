ad5d55889c8edf9bf3be3b8d45792294ab821dcd
Summer <salvadin2@gmail.com>
2020-06-07 01:26:56 -0400

    slight design changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,24 @@
 
     window.initialize = () => {
         // Automagically grab my repositories and populate #repos with them.
-        window.fetch('https://api.github.com/users/alerithe/repos').then(res => res.json()).then(repos => {
-            let reposList = document.getElementById('repos');
-            for (let repo of repos) {
-                let repoItem = document.createElement('li');
-                repoItem.innerHTML = `<a href="${repo.html_url}" target="_blank">${repo.name}</a> - ${repo.description}`;
-                reposList.appendChild(repoItem);
+        let repoList = document.getElementById('repos');
+        window.fetch('https://api.github.com/users/alerithe/repos', {
+            cache: 'no-cache'
+        }).then(response => {
+            repoList.children[0].remove();
+
+            if (response.ok) {
+                response.json().then(repos => {
+                    for (let repo of repos) {
+                        let repoItem = document.createElement('li');
+                        repoItem.innerHTML = `<a href="${repo.html_url}" target="_blank">${repo.name}</a> - ${repo.description}`;
+                        repoList.appendChild(repoItem);
+                    }
+                });
+            } else {
+                let errorMessage = document.createElement('li');
+                errorMessage.innerText = "There was an issue grabbing the repository list...";
+                repoList.appendChild(errorMessage);
             }
         });
 
