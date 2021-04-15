02a7bde15e042079783c1d195eb208f34b20e5ba
Summer <salvadin2@gmail.com>
2021-04-15 01:17:09 -0400

    new style in the works

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,38 @@
-/*
- * So you've decided to check out the code for my website, cool.
- */
+// So you've decided to check out the code for my website, cool.
 (function () {
     'use strict';
 
     window.initialize = () => {
-        // Retrieve my GitHub repositories and populate #repos with links and descriptions for them.
-        let repoList = document.getElementById('repos');
-        
-        window.fetch('https://api.github.com/users/alerithe/repos').then(res => {
-            if (res.ok) { // Everything went smoothly, hooray!
-                // Convert the response to a JSON array.
-                res.json().then(repos => {
-                    for (let repo of repos) {
-                        let repoItem = document.createElement('li');
-                        repoItem.innerHTML = `<a href="${repo.html_url}" target="_blank">${repo.name}</a> - <em>${repo.description}</em>`;
-                        repoList.appendChild(repoItem);
-                    }
-                });
-            } else { // Something happened, but I'm not sure what.
-                let errorItem = document.createElement('li');
-                errorItem.innerText = 'An issue occured retrieving my repository list...';
-                repoList.appendChild(errorItem);
-            }
-        }).catch(e => { // An exception was thrown, report it here.
-            let errorItem = document.createElement('li');
-            errorItem.innerText = `The following error occured while retrieving my repository list...\n\n${e}`;
-            repoList.appendChild(errorItem);
-        });
-
         // Make external links open in a new tab.
         for (let link of document.getElementsByTagName('a')) {
             if (link.hostname !== location.hostname) {
                 link.target = '_blank';
             }
         }
+
+        // Retrieve my GitHub repositories and populate #repos with links and descriptions for them.
+        let repoListElem = document.getElementById('repositories');
+
+        window.fetch('https://api.github.com/users/alerithe/repos').then(response => {
+            if (response.ok) { // Everything went smoothly, hooray!
+                // Convert the response to a JSON array.
+                response.json().then(repositories => {
+                    for (let repo of repositories) {
+                        let repoElem = document.createElement('div');
+                        repoElem.classList.add('repository');
+                        repoElem.innerHTML = `<p class="repo-title"><a href="${repo.html_url}" target="_blank">${repo.name}</a></p><em>${repo.description}</em>`;
+                        repoListElem.appendChild(repoElem);
+                    }
+                });
+            } else { // Something happened, but I'm not sure what.
+                let errorElem = document.createElement('div');
+                errorElem.innerText = 'An issue occured retrieving my repository list...';
+                repoListElem.appendChild(errorElem);
+            }
+        }).catch(e => { // An exception was thrown, report it here.
+            let errorElem = document.createElement('div');
+            errorElem.innerText = `The following error occured while retrieving my repository list...\n\n${e}`;
+            repoListElem.appendChild(errorElem);
+        });
     };
 })();
