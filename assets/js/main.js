fc14808dbc6ee044af6d19c7b885c726a11cd037
Summer <salvadin2@gmail.com>
2021-11-21 05:23:16 -0500

    woah github pages update??

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,7 @@
-// So you've decided to check out the code for my website, cool.
 (function () {
     'use strict';
 
-    window.initialize = () => {
+    window.doPostInitTasks = () => {
         // Make external links open in a new tab.
         for (let link of document.getElementsByTagName('a')) {
             if (link.hostname !== location.hostname) {
@@ -13,7 +12,7 @@
         // Retrieve my GitHub repositories and populate #repos with links and descriptions for them.
         let repoListElem = document.getElementById('repositories');
         window.fetch('https://api.github.com/users/alerithe/repos').then(response => {
-            if (response.ok) { // Everything went smoothly, hooray!
+            if (response.ok) {
                 // Convert the response to a JSON array.
                 response.json().then(repositories => {
                     for (let repo of repositories) {
@@ -28,7 +27,7 @@
                 errorElem.innerText = 'An issue occured retrieving my repository list...';
                 repoListElem.appendChild(errorElem);
             }
-        }).catch(e => { // An exception was thrown, report it here.
+        }).catch(e => { // An exception was thrown, report it.
             let errorElem = document.createElement('div');
             errorElem.innerText = `The following error occured while retrieving my repository list...\n\n${e}`;
             repoListElem.appendChild(errorElem);
