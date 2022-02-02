2f94ceb9aad2f6a3c5f5f07eb2ba7622f1e7f5b4
Tiva <salvadin2@gmail.com>
2022-02-01 20:48:15 -0500

    update github api endpoint

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,17 @@
     'use strict';
 
     window.doPostInitTasks = () => {
-        // Make external links open in a new tab.
         for (let link of document.getElementsByTagName('a')) {
             if (link.hostname !== location.hostname) {
                 link.target = '_blank';
             }
         }
 
-        // Retrieve my GitHub repositories and populate #repos with links and descriptions for them.
         let repoListElem = document.getElementById('repositories');
-        window.fetch('https://api.github.com/users/suhtiva/repos').then(response => {
+        window.fetch('https://api.github.com/users/tivuhh/repos', {
+            headers: { "User-Agent": navigator.userAgent + " (tivuhh.github.io)" }
+        }).then(response => {
             if (response.ok) {
-                // Convert the response to a JSON array.
                 response.json().then(repositories => {
                     for (let repo of repositories) {
                         let repoElem = document.createElement('div');
@@ -23,12 +22,12 @@
                         repoListElem.appendChild(repoElem);
                     }
                 });
-            } else { // Something happened, but I'm not sure what.
+            } else {
                 let errorElem = document.createElement('div');
                 errorElem.innerText = 'An issue occured retrieving my repository list...';
                 repoListElem.appendChild(errorElem);
             }
-        }).catch(e => { // An exception was thrown, report it.
+        }).catch(e => {
             let errorElem = document.createElement('div');
             errorElem.innerText = `The following error occured while retrieving my repository list...\n\n${e}`;
             repoListElem.appendChild(errorElem);
