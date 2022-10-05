2dd08ff6839ddca47d06f5761e6c194f24a650fa
Hannah <salvadin2@gmail.com>
2022-10-05 02:53:37 -0400

    begin restyle/overhaul

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,30 +7,5 @@
                 link.target = '_blank';
             }
         }
-
-        let repoListElem = document.getElementById('repositories');
-        window.fetch('https://api.github.com/users/winnpixie/repos', {
-            headers: { "User-Agent": navigator.userAgent + " (winnpixie.github.io)" }
-        }).then(response => {
-            if (response.ok) {
-                response.json().then(repositories => {
-                    for (let repo of repositories) {
-                        let repoElem = document.createElement('div');
-                        repoElem.classList.add('repository');
-                        repoElem.innerHTML = '<p class="repo-title">'
-                            + `<a href="${repo.html_url}" target="_blank">${repo.name}</a></p>${repo.description}`;
-                        repoListElem.appendChild(repoElem);
-                    }
-                });
-            } else {
-                let errorElem = document.createElement('div');
-                errorElem.innerText = 'An issue occured retrieving my repository list...';
-                repoListElem.appendChild(errorElem);
-            }
-        }).catch(e => {
-            let errorElem = document.createElement('div');
-            errorElem.innerText = `The following error occured while retrieving my repository list...\n\n${e}`;
-            repoListElem.appendChild(errorElem);
-        });
     };
 })();
